f4d54e6685a21f127c2436e7119eb17bbcdb02df
Craig Miller <craigmiller160@gmail.com>
2021-04-18 11:11:19 -0400

    More work on prettier

--- src/Hello.js
+++ src/Hello.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-const Hello = (props) => (
-	<div>
-		<h1>{props.message}</h1>
-	</div>
-);
+const Hello = (props) => {
+	useEffect(() => {
+		const object = { abc: 'def' };
+		console.log(object);
+		const array = ['one', 'two'];
+		console.log(array);
+	}, []);
+
+	return (
+		<div>
+			<h1>{props.message}</h1>
+			<button id="foo">ABC</button>
+		</div>
+	);
+};
 Hello.propTypes = {
 	message: PropTypes.string.isRequired
 };
