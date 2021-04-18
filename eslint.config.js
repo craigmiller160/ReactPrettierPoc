3a1044a90add73defc2f4cf2315f61ebb81dff43
Craig <craigmiller160@gmail.com>
2021-04-18 10:55:10 -0400

    Working on eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,13 @@
 
 module.exports = {
-
+    root: true,
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
+        }
+    }
 };
 
 // https://dev.to/onygami/eslint-and-prettier-for-react-apps-bonus-next-js-and-typescript-3e46
